fd7d16e61d668c404e5bc22129f2e5ebfed47f87
ajamil-techrayslabs <ajamil@techrayslabs.com>
2026-01-16 18:21:41 +0530

    adding kaisa laga mera majak

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
       <h1>Github Actions</h1>
       <h2>Complete course with 2 projects</h2>
       <h3>This is automated deployment</h3>
+      <h4>kaisa laga mera majak</h4>
     </>
   )
 }
